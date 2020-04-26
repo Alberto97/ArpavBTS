4cf187fca19ce5e8b1d2028e681b76a2242c46dd
Alberto Pedron <albertop2197@gmail.com>
2020-04-26 21:08:57 +0200

    Drop Protobuf stuff
    I really liked the idea but it's too much hassle right now and the serializer is not stable yet

--- app/src/main/java/org/alberto97/arpavbts/modules/AppModule.kt
+++ app/src/main/java/org/alberto97/arpavbts/modules/AppModule.kt
@@ -1,7 +1,5 @@
 package org.alberto97.arpavbts.modules
 
-import org.alberto97.arpavbts.repositories.BtsRepository
-import org.alberto97.arpavbts.repositories.IBtsRepository
 import org.alberto97.arpavbts.tools.GestoriUtils
 import org.alberto97.arpavbts.tools.IGestoriUtils
 import org.alberto97.arpavbts.viewmodels.MapViewModel
@@ -10,10 +8,6 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-
-    single<IBtsRepository> {
-        BtsRepository(get(), get())
-    }
 
     single<IGestoriUtils> { GestoriUtils() }
 
