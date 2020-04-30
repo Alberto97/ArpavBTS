ce329ec854234b396137ca736f4c14dfe1a49d8b
Alberto Pedron <albertop2197@gmail.com>
2020-04-30 21:25:53 +0200

    Remove unused viewModel

--- app/src/main/java/org/alberto97/arpavbts/modules/AppModule.kt
+++ app/src/main/java/org/alberto97/arpavbts/modules/AppModule.kt
@@ -3,7 +3,6 @@ package org.alberto97.arpavbts.modules
 import org.alberto97.arpavbts.tools.GestoriUtils
 import org.alberto97.arpavbts.tools.IGestoriUtils
 import org.alberto97.arpavbts.viewmodels.MapViewModel
-import org.alberto97.arpavbts.viewmodels.StartViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -12,5 +11,4 @@ val appModule = module {
     single<IGestoriUtils> { GestoriUtils() }
 
     viewModel { MapViewModel(get(), get(), get()) }
-    viewModel { StartViewModel() }
 }
--- app/src/main/java/org/alberto97/arpavbts/viewmodels/StartViewModel.kt
+++
@@ -1,5 +0,0 @@
-package org.alberto97.arpavbts.viewmodels
-
-import androidx.lifecycle.ViewModel
-
-class StartViewModel : ViewModel()
