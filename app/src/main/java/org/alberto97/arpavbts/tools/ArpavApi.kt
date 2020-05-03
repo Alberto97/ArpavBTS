a3a28bdb15a8f08f231a6ba27fc81ef0b4a79458
Alberto Pedron <albertop2197@gmail.com>
2020-05-03 13:46:30 +0200

    Use gradle to read secrets

--- app/src/main/java/org/alberto97/arpavbts/tools/ArpavApi.kt
+++ app/src/main/java/org/alberto97/arpavbts/tools/ArpavApi.kt
@@ -1,10 +1,11 @@
 package org.alberto97.arpavbts.tools
 
+import org.alberto97.arpavbts.BuildConfig
 import org.alberto97.arpavbts.models.arpav.FeatureCollection
 import retrofit2.http.GET
 
 interface ArpavApi {
 
-    @GET("http://alberto97.altervista.org/arpav/v1/impianti.json")
+    @GET(BuildConfig.ENDPOINT_URL)
     suspend fun fetchData(): FeatureCollection
 }
