a4b348ada0acf0aeb124f9ab3cacb7316333a9b7
Alberto Pedron <albertop2197@gmail.com>
2020-08-27 16:07:54 +0200

    Update Koin to v2.1.6

--- app/src/main/java/org/alberto97/arpavbts/MainApplication.kt
+++ app/src/main/java/org/alberto97/arpavbts/MainApplication.kt
@@ -7,6 +7,7 @@ import org.alberto97.arpavbts.modules.networkModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 @Suppress("unused")
 class MainApplication : Application() {
@@ -14,7 +15,7 @@ class MainApplication : Application() {
         super.onCreate()
         startKoin {
             if (BuildConfig.DEBUG)
-                androidLogger()
+                androidLogger(Level.ERROR)
 
             // Android context
             androidContext(this@MainApplication)
