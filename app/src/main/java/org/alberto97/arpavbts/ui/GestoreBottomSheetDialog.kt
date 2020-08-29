2ff9feb29e28cbe78314f5325f02772402f91ba4
Alberto Pedron <albertop2197@gmail.com>
2020-08-29 13:13:28 +0200

    Migrate GestoreBottomSheetDialog to Compose

--- app/src/main/java/org/alberto97/arpavbts/ui/GestoreBottomSheetDialog.kt
+++ app/src/main/java/org/alberto97/arpavbts/ui/GestoreBottomSheetDialog.kt
@@ -2,22 +2,31 @@ package org.alberto97.arpavbts.ui
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.alberto97.arpavbts.R
-import org.alberto97.arpavbts.adapters.GestoreAdapter
-import org.alberto97.arpavbts.databinding.DialogGestoreBinding
 import org.alberto97.arpavbts.models.GestoreAdapterItem
 import org.alberto97.arpavbts.tools.*
 
 const val SHEET_SELECTED_PROVIDER = "Provider"
 class GestoreBottomSheetDialog : BottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = DialogGestoreBinding.inflate(inflater, container, false)
 
         val list = arrayListOf(
             getItem(getString(R.string.provider_all), null),
@@ -27,23 +36,44 @@ class GestoreBottomSheetDialog : BottomSheetDialogFragment() {
             getItem(getString(R.string.provider_iliad), iliadName)
         )
 
-        val adapter = GestoreAdapter { gestore -> onGestoreClick(gestore) }
-        binding.recyclerView.adapter = adapter
-        adapter.submitList(list)
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MaterialTheme {
+                    Column {
+                        Text(
+                            text = stringResource(R.string.provider_pick),
+                            modifier = Modifier.padding(15.dp)
+                        )
 
-        return binding.root
+                        LazyColumnFor(list) {
+                            ListItemGestore(
+                                id = it.id,
+                                text = it.name,
+                                tint = Color(it.color),
+                                onClick = { id -> onGestoreClick(id) }
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    private fun onGestoreClick(data: GestoreAdapterItem) {
+    private fun onGestoreClick(id: String?) {
         val intent = Intent()
-        intent.putExtra(SHEET_SELECTED_PROVIDER, data.id)
+        intent.putExtra(SHEET_SELECTED_PROVIDER, id)
         targetFragment?.onActivityResult(targetRequestCode, Activity.RESULT_OK, intent)
-        this.dismiss()
+
+        // Delay dismiss to allow Compose properly ending its job
+        lifecycleScope.launch {
+            delay(500L)
+            super.dismiss()
+        }
     }
 
     private fun getItem(name: String, id: String?): GestoreAdapterItem {
         val colorStr = carrierColor[id] ?: allColor
-        val color = Color.parseColor(colorStr)
+        val color = android.graphics.Color.parseColor(colorStr)
 
         return GestoreAdapterItem(color, name, id)
     }
---
+++ app/src/main/java/org/alberto97/arpavbts/ui/ListItemGestore.kt
@@ -0,0 +1,60 @@
+package org.alberto97.arpavbts.ui
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope.gravity
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+
+@Composable
+fun ListItemGestore(id: String?, text: String, tint: Color, onClick: (id: String?) -> Unit) {
+    Row(
+        Modifier.fillMaxWidth()
+            .clickable(onClick = {
+                onClick(id)
+            })
+            .padding(15.dp)
+            .gravity(Alignment.CenterVertically)
+    ) {
+        Surface(
+            color = tint,
+            modifier = Modifier.size(30.dp).clip(CircleShape)
+                .clickable(onClick = {
+                    onClick(id)
+                })
+        ) {}
+        Text(
+            text = text,
+            style = MaterialTheme.typography.subtitle1,
+            maxLines = 1,
+            modifier = Modifier
+                .gravity(Alignment.CenterVertically)
+                .padding(start = 15.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ListItemPreview() {
+    MaterialTheme {
+        ListItemGestore(
+            id = "1",
+            text = "Prova",
+            tint = Color(0xff1faa00),
+            onClick = {}
+        )
+    }
+}
