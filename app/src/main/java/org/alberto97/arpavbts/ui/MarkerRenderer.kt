7db67e5c0299d1a2f78216bfab7a079d5e5a03b7
Alberto Pedron <albertop2197@gmail.com>
2020-09-02 17:12:57 +0200

    MarkerRenderer: Resolve GestoriUtils with Koin

--- app/src/main/java/org/alberto97/arpavbts/ui/MarkerRenderer.kt
+++ app/src/main/java/org/alberto97/arpavbts/ui/MarkerRenderer.kt
@@ -7,14 +7,15 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.clustering.view.DefaultClusterRenderer
 import org.alberto97.arpavbts.models.ClusterItemData
-import org.alberto97.arpavbts.tools.GestoriUtils
 import org.alberto97.arpavbts.tools.Extensions.toHue
+import org.alberto97.arpavbts.tools.IGestoriUtils
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
 class MarkerRenderer(context: Context, map: GoogleMap, clusterManager: ClusterManager<ClusterItemData>) :
-    DefaultClusterRenderer<ClusterItemData>(context, map, clusterManager) {
+    DefaultClusterRenderer<ClusterItemData>(context, map, clusterManager), KoinComponent {
 
-    // TODO: Figure out how to properly inject IGestoriUtils with Koin here
-    private val utils = GestoriUtils()
+    private val utils: IGestoriUtils by inject()
 
     override fun onBeforeClusterItemRendered(item: ClusterItemData, markerOptions: MarkerOptions) {
         val color = utils.getColor(item.data.gestore).toHue()
