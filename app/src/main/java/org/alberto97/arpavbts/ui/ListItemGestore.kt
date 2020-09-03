b204efa8d215a02440301be18a06df8957592012
Alberto Pedron <albertop2197@gmail.com>
2020-09-03 15:42:21 +0200

    Use Material ListItem for ListItemGestore

--- app/src/main/java/org/alberto97/arpavbts/ui/ListItemGestore.kt
+++ app/src/main/java/org/alberto97/arpavbts/ui/ListItemGestore.kt
@@ -2,16 +2,12 @@ package org.alberto97.arpavbts.ui
 
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope.gravity
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -21,28 +17,21 @@ import org.alberto97.arpavbts.ui.theme.AppTheme
 
 @Composable
 fun ListItemGestore(id: String?, text: String, tint: Color, onClick: (id: String?) -> Unit) {
-    Row(
-        Modifier.fillMaxWidth()
-            .clickable(onClick = {
-                onClick(id)
-            })
-            .padding(15.dp)
-            .gravity(Alignment.CenterVertically)
+    ListItem(
+        modifier = Modifier.clickable(
+            onClick = { onClick(id) }
+        ),
+        icon = {
+            Surface(
+                color = tint,
+                modifier = Modifier.size(30.dp).clip(CircleShape)
+            ) {}
+        },
     ) {
-        Surface(
-            color = tint,
-            modifier = Modifier.size(30.dp).clip(CircleShape)
-                .clickable(onClick = {
-                    onClick(id)
-                })
-        ) {}
         Text(
             text = text,
             style = MaterialTheme.typography.subtitle1,
-            maxLines = 1,
-            modifier = Modifier
-                .gravity(Alignment.CenterVertically)
-                .padding(start = 15.dp)
+            maxLines = 1
         )
     }
 }
