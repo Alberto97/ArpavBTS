6969072de60d96287ef90e441ee85303a4577e4e
Alberto Pedron <albertop2197@gmail.com>
2021-10-12 17:21:32 +0200

    DownloadWorker: Log exception

--- app/src/main/java/org/alberto97/arpavbts/workers/DownloadWorker.kt
+++ app/src/main/java/org/alberto97/arpavbts/workers/DownloadWorker.kt
@@ -6,6 +6,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.hilt.work.HiltWorker
@@ -42,6 +43,7 @@ class DownloadWorker @AssistedInject constructor(
                 btsRepo.updateBtsIfOldOrEmpty()
             }
         } catch (e: Exception) {
+            Log.e("DownloadWorker", e.stackTraceToString())
             return Result.failure()
         }
 
