142ddb937a924e763936714679817631bcf12d96
Alberto Pedron <albertop2197@gmail.com>
2021-10-12 17:40:22 +0200

    Export room database schema

--- app/src/main/java/org/alberto97/arpavbts/db/AppDb.kt
+++ app/src/main/java/org/alberto97/arpavbts/db/AppDb.kt
@@ -5,8 +5,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [Bts::class],
-    version = 1,
-    exportSchema = false
+    version = 1
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun btsDao(): BtsDao
