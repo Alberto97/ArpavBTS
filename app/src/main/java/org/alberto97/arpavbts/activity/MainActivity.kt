bf05173eee22e23ced2faf8785d06e423a082e3b
Alberto Pedron <albertop2197@gmail.com>
2021-10-12 18:51:24 +0200

    Add splash screen for Android 12

--- app/src/main/java/org/alberto97/arpavbts/activity/MainActivity.kt
+++ app/src/main/java/org/alberto97/arpavbts/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package org.alberto97.arpavbts.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import org.alberto97.arpavbts.R
 import org.alberto97.arpavbts.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setTheme(R.style.AppTheme)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
