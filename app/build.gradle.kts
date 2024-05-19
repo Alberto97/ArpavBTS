39e41e61a7ae4d707cb3e5aef4721e6d541151de
Alberto Pedron <albertop2197@gmail.com>
2024-05-19 22:38:39 +0200

    Use first-party Retrofit converter for Kotlinx Serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,7 +97,7 @@ dependencies {
     val retrofitVersion = "2.11.0"
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:$retrofitVersion")
 
     // Work
     val workVersion = "2.9.0"
